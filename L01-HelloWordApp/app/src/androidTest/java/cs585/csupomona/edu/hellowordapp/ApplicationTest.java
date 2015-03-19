06bb86a21208b77643f337cc742736bd43a8897a
Yu Sun <yu.sun.cs@gmail.com>
2015-03-19 15:17:39 -0700

    L01 HelloWorld

---
+++ L01-HelloWordApp/app/src/androidTest/java/cs585/csupomona/edu/hellowordapp/ApplicationTest.java
@@ -0,0 +1,13 @@
+package cs585.csupomona.edu.hellowordapp;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
