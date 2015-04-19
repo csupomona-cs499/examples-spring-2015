b6a84d04e4b8775b98c94646175d030ab06fbf96
Yu Sun <yu.sun.cs@gmail.com>
2015-04-19 11:53:57 -0700

    clean up the class demo for basic widgets

--- L04-Basic-Widgets/app/src/main/java/cs499/cpp/edu/l04_basic_widgets/MainActivity.java
+++ L04-Basic-Widgets/app/src/main/java/cs499/cpp/edu/l04_basic_widgets/MainActivity.java
@@ -2,6 +2,8 @@ package cs499.cpp.edu.l04_basic_widgets;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -33,6 +35,22 @@ public class MainActivity extends ActionBarActivity {
         // 2. EditText
         final EditText editText = (EditText) findViewById(R.id.editTextDemo);
         Log.i(TAG, editText.getText().toString()); // get text
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                textView.setText(s.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
 
         // 3. Button
         final TextView resultView = (TextView) findViewById(R.id.textResult);
