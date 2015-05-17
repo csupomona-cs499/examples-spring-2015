00165426d011d4c50bdae6fa280e191794460435
Yu Sun <yu.sun.cs@gmail.com>
2015-05-16 23:16:56 -0700

    broadcast recevier

---
+++ L18-Broadcast-Receiver/app/src/main/java/edu/cpp/cs499/l17_broadcast_receiver/NetworkStatusReceiver.java
@@ -0,0 +1,62 @@
+package edu.cpp.cs499.l17_broadcast_receiver;
+
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
+
+/**
+ * Created by yusun on 5/16/15.
+ */
+public class NetworkStatusReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Log.i("TEST", "Received the intent: " + intent.getAction());
+        if (intent.getAction().equals(ConnectivityManager.CONNECTIVITY_ACTION)) {
+            ConnectivityManager connectivityManager =
+                    (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+            NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+            boolean isConnected = (networkInfo != null) && networkInfo.isConnected();
+            Log.i("TEST", "Network status: " + isConnected);
+            sendNotification(context, isConnected);
+        }
+    }
+
+    private void sendNotification(Context context, boolean isConnected) {
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context)
+                .setSmallIcon(android.R.drawable.ic_dialog_alert)
+                .setContentTitle("Wifi Status Changed")
+                .setContentText("Current Status: " + isConnected);
+
+        // Creates an explicit intent for an Activity in your app
+        Intent resultIntent = new Intent(context, MainActivity.class);
+
+        // The stack builder object will contain an artificial back stack for the
+        // started Activity.
+        // This ensures that navigating backward from the Activity leads out of
+        // your application to the Home screen.
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
+        // Adds the back stack for the Intent (but not the Intent itself)
+        stackBuilder.addParentStack(MainActivity.class);
+        // Adds the Intent that starts the Activity to the top of the stack
+        stackBuilder.addNextIntent(resultIntent);
+        PendingIntent resultPendingIntent =
+                stackBuilder.getPendingIntent(
+                        0,
+                        PendingIntent.FLAG_UPDATE_CURRENT
+                );
+        builder.setContentIntent(resultPendingIntent);
+
+        NotificationManager notificationManager = (NotificationManager)
+                context.getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(0, builder.build());
+    }
+
+}
